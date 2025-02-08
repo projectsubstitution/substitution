82cfc403514028239836d7e710dad9ec1b5ca8d6
projectsubstitution <projectsubstitution@gmail.com>
2025-02-08 14:31:01 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'; // Import the React plugin for Vite
+import react from '@vitejs/plugin-react';
 
-// Export the Vite configuration
 export default defineConfig({
-  plugins: [react()], // Use the React plugin
-  server: {
-    port: 3000, // Specify the development server port
-  },
-  build: {
-    outDir: 'dist', // Specify the output directory for the build
-  },
+  plugins: [react()],
+  base: '/<substitution>/', // Replace <REPOSITORY_NAME> with your actual repository name
 });
