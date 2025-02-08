5b5017aa17263a878ade6923e9b2a1955def0547
projectsubstitution <projectsubstitution@gmail.com>
2025-02-08 14:37:00 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/<substitution>/', // Replace <REPOSITORY_NAME> with your actual repository name
+  base: '/substitution/', // Replace <REPOSITORY_NAME> with your actual repository name
 });
